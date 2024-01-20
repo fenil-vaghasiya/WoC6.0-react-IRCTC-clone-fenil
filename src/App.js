e843024b4641f54be560d623417319f6659aac50
fenil-vaghasiya <fenilvaghasiya2026@gmail.com>
2024-01-21 01:08:48 +0530

    Sign-up, Login and authentication functionality added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { app } from './firebase';
+import { useEffect, useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+  const [user,setUser]=useState(null)
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,user=>{
+      if(user){
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    })
+  },[])
+
+  if(user != null){
+    return(
+      <div className='flex flex-col justify-center items-center p-2'>
+        <div>Hello {user.email}</div>
+        <button 
+          className='btn bg-blue-900 hover:bg-slate-500 text-white font-semibold'
+          onClick={()=>signOut(auth)}>SIGN OUT</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { app } from '../firebase'
+import { toast } from 'react-toastify'
+
+const auth = getAuth(app);
+
+function Login() {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const signinUser=()=>{
+        signInWithEmailAndPassword(auth,email,password)
+            .then(value=>toast.success("Login successfull",{position:"top-center"}))
+            .catch(e=>{
+                let errorcode = e.code.split("auth/")[1];
+                toast.error(errorcode,{position:"top-center"});
+            })
+    }
+  return (
+    <div className='sign-up'>
+        <div className='h-screen backdrop-blur-[3px] bg-slate-600/30 w-full flex justify-center items-center'>
+            <div className='w-1/3 h-3/6 rounded-3xl backdrop-blur-2xl bg-white/30'>
+                <div className='h-1/4 w-full flex flex-col justify-end items-center text-2xl font-bold'>
+                    <div className='rounded-full bg-blue-900 text-white w-7 h-7 text-sm font-bold text-center'><FontAwesomeIcon icon={faLock} /></div>
+                    <div>Sign In</div>
+                </div>
+                <div className='h-2/4 flex flex-col w-5/6 mx-auto justify-center'>
+                    <label htmlFor="email">Email address*</label>
+                    <input 
+                        type="email" 
+                        placeholder='Enter your email' 
+                        className='outline-none border-4 border-b-black border-none rounded-lg py-2 pl-2' 
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}/>
+                    <label htmlFor="password">Password*</label>
+                    <input 
+                        type="password" 
+                        placeholder='Enter your password'
+                        className='outline-none border border-b-black rounded-lg py-2 pl-2'
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}/>
+                    
+                </div>
+                <div className='h-1/4 text-md w-5/6 mx-auto text-white flex flex-col'>
+                    <button 
+                        className='btn bg-blue-900 hover:bg-slate-500 text-white font-semibold'
+                        onClick={()=>signinUser()}>SIGN IN</button>
+                    <a href="/signup" className='underline underline-offset-2'>DON'T HAVE AN ACCOUNT? SIGN UP</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
